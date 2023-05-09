814af8a66d2b990e8864ec600d24a3bde92dca07
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2023-05-09 21:36:22 +0300

    done for today

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { App } from 'components/App';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 
+//! https://youtu.be/f1jB13fK4dI?t=1676
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/lesson-05">
